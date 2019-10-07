d977ccc1fb3e221ca8c08a7404845149865ee0fa
TheMCBrothers <themcbrothers2016@gmail.com>
2019-10-07 23:44:37 +0200

    Changes to upgrades

--- src/main/java/tk/themcbros/uselessmod/items/UpgradeItem.java
+++ src/main/java/tk/themcbros/uselessmod/items/UpgradeItem.java
@@ -1,7 +1,15 @@
 package tk.themcbros.uselessmod.items;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
 import tk.themcbros.uselessmod.machine.Upgrade;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class UpgradeItem extends Item {
 	
@@ -16,4 +24,9 @@ public class UpgradeItem extends Item {
 		return upgrade;
 	}
 
+	@Override
+	public void addInformation(ItemStack stack, @Nullable World world, List<ITextComponent> tooltip, ITooltipFlag flag) {
+		if(this.upgrade.hasTranslationKey())
+			tooltip.add(new TranslationTextComponent(this.upgrade.getTranslationKey()));
+	}
 }
--- src/main/java/tk/themcbros/uselessmod/machine/Upgrade.java
+++ src/main/java/tk/themcbros/uselessmod/machine/Upgrade.java
@@ -2,26 +2,40 @@ package tk.themcbros.uselessmod.machine;
 
 import net.minecraft.util.IStringSerializable;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Locale;
+
 public enum Upgrade implements IStringSerializable {
 
-	NULL("null", 0),
-	SPEED("speed", 10);
-	
-	private final String name;
+	NULL(0, "tooltip.uselessmod.upgrade.null"),
+	SPEED(10, "tooltip.uselessmod.upgrade.speed");
+
 	private final int additionalEnergyUsage;
+	private final String translationKey;
 	
-	private Upgrade(String name, int additionalEnergyUsage) {
-		this.name = name;
+	private Upgrade(@Nonnull int additionalEnergyUsage, @Nullable String translationKey) {
 		this.additionalEnergyUsage = additionalEnergyUsage;
+		this.translationKey = translationKey;
 	}
 	
 	@Override
+	@Nonnull
 	public String getName() {
-		return name;
+		return name().toLowerCase(Locale.ROOT);
 	}
-	
+
+	@Nonnull
 	public int getAdditionalEnergyUsage() {
 		return additionalEnergyUsage;
 	}
-	
+
+	@Nullable
+    public String getTranslationKey() {
+		return this.translationKey;
+    }
+
+	public boolean hasTranslationKey() {
+		return this.translationKey != null && !this.translationKey.isEmpty();
+	}
 }
