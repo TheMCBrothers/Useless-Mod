fed4ef303c38cd7a0161b2245f340ee36e863bd3
TheMCBrothers <themcbrothers2016@gmail.com>
2019-10-08 01:11:52 +0200

    Changes to PowerControlBlock

--- src/main/java/tk/themcbros/uselessmod/tileentity/PowerControlTileEntity.java
+++ src/main/java/tk/themcbros/uselessmod/tileentity/PowerControlTileEntity.java
@@ -1,13 +1,5 @@
 package tk.themcbros.uselessmod.tileentity;
 
-import java.util.Map;
-import java.util.Set;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
-import com.google.common.collect.Maps;
-
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
@@ -15,16 +7,15 @@ import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.energy.CapabilityEnergy;
 import tk.themcbros.uselessmod.blocks.PowerControlBlock;
-import tk.themcbros.uselessmod.blocks.PowerControlBlock.PowerSide;
 import tk.themcbros.uselessmod.energy.CustomEnergyStorage;
 import tk.themcbros.uselessmod.lists.ModTileEntities;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class PowerControlTileEntity extends TileEntity implements ITickableTileEntity {
 
 	private CustomEnergyStorage energyStorage;
-	
-	@SuppressWarnings("unused")
-	private final Map<PowerSide, Set<Direction>> sides = Maps.newHashMap();
 	
 	public PowerControlTileEntity() {
 		super(ModTileEntities.POWER_CONTROL_BLOCK);
@@ -37,19 +28,15 @@ public class PowerControlTileEntity extends TileEntity implements ITickableTileE
 	public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
 		if(cap == CapabilityEnergy.ENERGY && side != null && this.canConnect(side))
 			return this.energyHolder.cast();
-		return super.getCapability(cap, side);
+		return LazyOptional.empty();
 	}
 
 	public boolean canConnect(@Nonnull Direction side) {
-		final PowerControlBlock block = (PowerControlBlock) this.getBlockState().getBlock();
-		if(!block.sideFromSide(this.getBlockState(), side).equals(PowerSide.NONE))
-			return true;
-		return false;
+		return PowerControlBlock.getConnection(this.getBlockState(), side).canConnect();
 	}
 
 	@Override
 	public void tick() {
-		
 	}
 
 }
