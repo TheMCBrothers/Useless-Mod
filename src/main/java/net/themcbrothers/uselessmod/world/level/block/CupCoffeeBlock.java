4d97f3711a332976c013424919c4991ce1302295
TheMCLoveMan <themcloveman@gmail.com>
2023-02-23 06:52:15 +0100

    fix coffee cup clone stack

--- src/main/java/net/themcbrothers/uselessmod/world/level/block/CupCoffeeBlock.java
+++ src/main/java/net/themcbrothers/uselessmod/world/level/block/CupCoffeeBlock.java
@@ -1,10 +1,16 @@
 package net.themcbrothers.uselessmod.world.level.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.HitResult;
 import net.themcbrothers.uselessmod.init.ModBlockEntityTypes;
+import net.themcbrothers.uselessmod.util.CoffeeUtils;
+import net.themcbrothers.uselessmod.world.level.block.entity.CupBlockEntity;
 import org.jetbrains.annotations.Nullable;
 
 public class CupCoffeeBlock extends CupBlock implements EntityBlock {
@@ -16,5 +22,15 @@ public class CupCoffeeBlock extends CupBlock implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return ModBlockEntityTypes.CUP.get().create(pos, state);
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(BlockState state, HitResult target, BlockGetter level, BlockPos pos, Player player) {
+        if (level.getBlockEntity(pos) instanceof CupBlockEntity cupBlockEntity
+                && cupBlockEntity.getCoffeeType() != null) {
+            return CoffeeUtils.getCoffeeStack(cupBlockEntity.getCoffeeType());
+        }
+
+        return super.getCloneItemStack(state, target, level, pos, player);
     }
 }
