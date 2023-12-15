589ed347e6374ace36dd2aad08059ac3f7ef4ec2
TheMCLoveMan <themcloveman@gmail.com>
2023-12-15 22:07:12 +0100

    NeoForge 1.20.2 - remove JEI plugin

--- src/main/java/net/themcbrothers/uselessmod/compat/jei/package-info.java
+++
@@ -1,7 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package net.themcbrothers.uselessmod.compat.jei;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
